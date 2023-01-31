14ab9522a7f99be6654621ae588053a92a7f0c1e
AmanGH009 <aman.mandal.71334@gmail.com>
2023-01-31 10:57:22 +0545

    Added components and scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
+import './styles/app.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/layout/Header';
+
 function App() {
   return (
-    <div className="App">
-      This is the burger house app.
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
