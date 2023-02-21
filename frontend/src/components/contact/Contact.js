83a4a11fee7f9827de04f64f63689b3911319696
AmanGH009 <aman.mandal.71334@gmail.com>
2023-02-21 17:39:39 +0545

    added form

--- frontend/src/components/contact/Contact.js
+++ frontend/src/components/contact/Contact.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import burger from '../../assets/burger2.png';
 
 const contact = () => {
     return (
         <section className='contact'>
-            <motion.form>
+            <motion.form
+                initial={{
+                    x: '-100vw',
+                    opacity: 0
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1
+                }}
+                transition={{ delay: 0.4 }}
+            >
                 <h2>Contact Us</h2>
                 <input type="text" placeholder='Name' />
                 <input type="text" placeholder='Email' />
                 <textarea placeholder='Message...' cols="30" rows="10"></textarea>
                 <button type='submit'>Send</button>
             </motion.form>
+
+            <motion.div className='formBorder'>
+                <motion.div>
+                    <img src={burger} alt='Burger' />
+                </motion.div>
+            </motion.div>
         </section>
     )
 }
