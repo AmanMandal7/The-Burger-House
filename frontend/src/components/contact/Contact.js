b2e3c7369b3582b31a3eef4f5694ad46880bee4b
AmanGH009 <aman.mandal.71334@gmail.com>
2023-02-21 17:44:30 +0545

    added animation

--- frontend/src/components/contact/Contact.js
+++ frontend/src/components/contact/Contact.js
@@ -23,7 +23,17 @@ const contact = () => {
                 <button type='submit'>Send</button>
             </motion.form>
 
-            <motion.div className='formBorder'>
+            <motion.div className='formBorder'
+                initial={{
+                    x: '100vw',
+                    opacity: 0
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1
+                }}
+                transition={{ delay: 0.2 }}
+            >
                 <motion.div>
                     <img src={burger} alt='Burger' />
                 </motion.div>
