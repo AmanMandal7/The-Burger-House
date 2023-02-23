86b777fa641d7d1db65f5692ee4aef4a3fbe95b0
AmanGH009 <aman.mandal.71334@gmail.com>
2023-02-23 08:31:36 +0545

    Implementing cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-import Contact from './components/contact/Contact'
+import Contact from './components/contact/Contact';
+import Cart from './components/cart/Cart';
 
 import './styles/app.scss';
 import './styles/header.scss'
@@ -11,6 +12,8 @@ import './styles/founder.scss'
 import './styles/menu.scss'
 import './styles/footer.scss'
 import './styles/contact.scss'
+import './styles/cart.scss'
+
 
 
 function App() {
@@ -20,6 +23,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
 
       <Footer />
