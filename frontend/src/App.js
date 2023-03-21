8cfcebd21b0e8feb72c247ff351c90f31c5c00fc
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-21 10:15:49 +0545

    starting with the confirm order section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/layout/Footer';
 import Contact from './components/contact/Contact';
 import Cart from './components/cart/Cart';
 import Shipping from './components/cart/Shipping';
+import ConfirmOrder from './components/cart/ConfirmOrder';
 
 import './styles/app.scss';
 import './styles/header.scss'
@@ -15,8 +16,7 @@ import './styles/footer.scss'
 import './styles/contact.scss'
 import './styles/cart.scss'
 import './styles/shipping.scss'
-
-
+import './styles/confirmOrder.scss'
 
 
 function App() {
@@ -28,6 +28,7 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/shipping' element={<Shipping />} />
+        <Route path='/confirmOrder' element={<ConfirmOrder />} />
       </Routes>
 
       <Footer />
