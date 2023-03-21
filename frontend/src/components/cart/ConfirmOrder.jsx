5a6b2d3fa4d56e0d7b561a4216b923038b173e7d
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-21 10:43:22 +0545

    done with the confirm order and its responsiveness

--- frontend/src/components/cart/ConfirmOrder.jsx
+++ frontend/src/components/cart/ConfirmOrder.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const ConfirmOrder = () => {
-  return <div></div>;
+  return (
+    <section className="confirmOrder">
+      <main>
+        <h1>Confir Order</h1>
+
+        <form>
+          <div>
+            <label>Cash On Delivery</label>
+            <input type="radio" name="payment" />
+          </div>
+          <div>
+            <label>Online Payment</label>
+            <input type="radio" name="payment" />
+          </div>
+
+          <button type="submit">Place Order</button>
+        </form>
+      </main>
+    </section>
+  );
 };
 
 export default ConfirmOrder;
