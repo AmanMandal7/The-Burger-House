f6a642d0479a0ab81a08d1075b10c8a7818deb15
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-23 11:15:01 +0545

    done with login with google page

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
-  return <section>This is Login page</section>;
+  return (
+    <section className="login">
+      <motion.button initial={{ y: "-100vh" }} animate={{ y: 0 }}>
+        Login with Google
+        <FcGoogle />
+      </motion.button>
+    </section>
+  );
 };
 
 export default Login;
