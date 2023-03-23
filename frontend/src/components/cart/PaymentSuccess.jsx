c89d47f535ae8b3fcbd909e5492346c276d60451
AmanGH009 <aman.mandal.71334@gmail.com>
2023-03-23 10:50:10 +0545

    done with payment success page

--- frontend/src/components/cart/PaymentSuccess.jsx
+++ frontend/src/components/cart/PaymentSuccess.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PaymentSuccess = () => {
-  return <div>Payment Success</div>;
+  return (
+    <section className="paymentSucess">
+      <main>
+        <h1>Order Confirmed</h1>
+        <p>Order Placed Successfully, You can check order status below</p>
+        <Link to="/myorders">Check Status</Link>
+      </main>
+    </section>
+  );
 };
 
 export default PaymentSuccess;
