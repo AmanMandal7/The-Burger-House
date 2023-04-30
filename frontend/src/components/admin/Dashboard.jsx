629fd05d5e0a98bcb382dccc4f4952d25dbf9a27
AmanGH009 <aman.mandal.71334@gmail.com>
2023-04-30 13:18:53 +0545

    dashboard css

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const Box = ({ title, value }) => (
+  <div>
+    <h3>
+      {title === "Income" && "₹"}
+      {value}
+    </h3>
+    <p>{title}</p>
+  </div>
+);
 
 const Dashboard = () => {
-  return <div>This is the dashboard</div>;
+  return (
+    <section className="dashboard">
+      <main>
+        <article>
+          <Box title="Users" value={123} />
+          <Box title="Orders" value={25} />
+          <Box title="Income" value={275000} />
+        </article>
+
+        <section>
+          <div>
+            <Link to="/admin/orders">View Orders</Link>
+            <Link to="/admin/users">View Users</Link>
+          </div>
+        </section>
+      </main>
+    </section>
+  );
 };
 
 export default Dashboard;
