de7f4e0c3d7e5b5b362d39104bccadba12687db0
AmanGH009 <aman.mandal.71334@gmail.com>
2023-05-02 10:58:20 +0545

    server is running on the port successfully

---
+++ server/server.js
@@ -0,0 +1,7 @@
+import app from "./app.js";
+
+app.get("/", (req, res, next) => {
+    res.send("<h1>Working</h1>");
+})
+
+app.listen(process.env.PORT, () => console.log(`Server is working on PORT: ${process.env.PORT}`));
