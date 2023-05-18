114427a2658fa638977f14f2cf9e2953fd44d829
AmanGH009 <aman.mandal.71334@gmail.com>
2023-05-18 10:08:39 +0530

    created order routes

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import { connectPassport } from "./utils/Provider.js"
 import session from "express-session";
@@ -21,6 +21,10 @@ app.use(session({
 }));
 
 app.use(cookieParser());
+app.use(express.json());
+app.use(urlencoded({
+    extended: true,
+}))
 
 app.use(passport.authenticate("session"));
 app.use(passport.initialize());
@@ -30,8 +34,10 @@ connectPassport();
 
 // Importing Routes
 import userRoute from "./routes/user.js";
+import orderRoute from "./routes/order.js";
 
 app.use("/api/v1", userRoute);
+app.use("/api/v1", orderRoute);
 
 
 // Using Error Middleware
