33b3bc59c8439063d88d5314dd69629385ba090a
AmanGH009 <aman.mandal.71334@gmail.com>
2023-05-19 16:59:35 +0530

    changing the order status successfully

--- server/routes/order.js
+++ server/routes/order.js
@@ -1,9 +1,16 @@
 import express from "express";
-// import { isAuthenticated } from "../middlewares/auth.js";
-import { placeOrder } from "../controllers/order.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import { getAdminOrders, getMyOrders, getOrderDetails, placeOrder, processOrder } from "../controllers/order.js";
 
 const router = express.Router();
 
-router.post("/createorder", placeOrder)
+router.post("/createorder", placeOrder);
+router.get("/myorders", isAuthenticated, getMyOrders);
+router.get("/order/:id", isAuthenticated, getOrderDetails);
+
+// Add Admin Middleware
+router.get("/admin/orders", isAuthenticated, getAdminOrders);
+router.get("/admin/order/:id", isAuthenticated, processOrder);
+
 
 export default router;
